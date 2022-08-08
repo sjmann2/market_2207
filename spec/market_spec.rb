78b13834ba27070691549ff896debe0ecfe5d69d
Sid Mann <sjmann2@outlook.com>
2022-08-08 11:09:17 -0500

    Iteration 4 tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -107,4 +107,43 @@ describe Market do
       expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
     end
   end
+
+  describe "Iteration 4" do
+    before :each do
+      @item1 = Item.new({name: 'Peach', price: "$0.75"})
+      @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+      @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      @item5 = Item.new({name: 'Onion', price: '$0.25'})
+      @market = Market.new("South Pearl Street Farmers Market")
+      vendor1 = Vendor.new("Rocky Mountain Fresh")
+      vendor1.stock(item1, 35)
+      vendor1.stock(item2, 7)
+      vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      vendor2.stock(item4, 50)
+      vendor2.stock(item3, 25)
+      vendor3 = Vendor.new("Palisade Peach Shack")
+      vendor3.stock(item1, 65)
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+
+
+    end
+
+    it "can give the date the market was created" do
+      allow(@market).to receive(:date).and_return(Date.parse("20200224").stftime("%d/%m/%Y"))
+      expect(@market.date).to eq("24/02/2020")
+    end
+
+    it "can sell items if there is enough of that item in stock across the market" do
+      expect(@market.sell(@item1, 200)).to be(false)
+      expect(@market.sell(@item5, 1)).to be(false)
+      expect(@market.sell(@item4, 5)).to be(true)
+      expect(@vendor2.check_stock(@item4)).to eq(45)
+      expect(@market.sell(@item1, 40)).to eq(true)
+      expect(@vendor1.check_stock(@item1)).to eq(0)
+      expect(@vendor3.check_stock(@item1)).to eq(60)
+    end
+  end
 end
