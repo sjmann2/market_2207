fbda4326742058790290109fa8c2d0f613d63032
Sid Mann <sjmann2@outlook.com>
2022-08-08 11:10:23 -0500

    fix syntax in iteration 4 tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -116,19 +116,17 @@ describe Market do
       @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
       @item5 = Item.new({name: 'Onion', price: '$0.25'})
       @market = Market.new("South Pearl Street Farmers Market")
-      vendor1 = Vendor.new("Rocky Mountain Fresh")
-      vendor1.stock(item1, 35)
-      vendor1.stock(item2, 7)
-      vendor2 = Vendor.new("Ba-Nom-a-Nom")
-      vendor2.stock(item4, 50)
-      vendor2.stock(item3, 25)
-      vendor3 = Vendor.new("Palisade Peach Shack")
-      vendor3.stock(item1, 65)
-      market.add_vendor(vendor1)
-      market.add_vendor(vendor2)
-      market.add_vendor(vendor3)
-
-
+      @vendor1 = Vendor.new("Rocky Mountain Fresh")
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      @vendor3 = Vendor.new("Palisade Peach Shack")
+      @vendor3.stock(@item1, 65)
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
     end
 
     it "can give the date the market was created" do
