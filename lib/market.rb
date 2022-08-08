19a5bc75c00f24f0db825a81ef10b5d8fc5394ad
Sid Mann <sjmann2@outlook.com>
2022-08-08 10:31:33 -0500

    passing tests for market class

--- lib/market.rb
+++ lib/market.rb
@@ -1,2 +1,23 @@
 class Market
+  attr_reader :name,
+              :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map(&:name)
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      vendor.inventory.include?(item)
+    end
+  end
 end
